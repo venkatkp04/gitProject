f5519c1a151021df663e6a48b1f10abc827ca58b
venkatkp04 <venkatkp04@gmail.com>
2025-12-19 12:10:06 +0530

    changed my OctetToDecimal file to print the right lyrics

--- OctetToDecimal.java
+++ OctetToDecimal.java
@@ -2,5 +2,6 @@ public class OctetToDecimal {
 
     public static void main(String[] args) {
         System.out.println("I am back in black. kendrick lamar kunfu kennny yo I put my life and love in this lines");
+        System.out.println("pulloff in my Roadstar sv haa. pockets full of ego rolling my eyes.");
     }
 }
